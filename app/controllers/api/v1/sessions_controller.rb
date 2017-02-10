906a3adec1f64db71d9310f042659f1ba6c38152
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 10:58:32 -0500

    set up sessions_controller to be more accurate

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,9 +5,11 @@ require 'Auth'
 class Api::V1::SessionsController < ApplicationController
 
   def login
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      render json: { token: Auth.create_token({ username: user.username, id: user.id, email: user.email }) }
+    user = User.find_by(email: params[:user][:email])
+    if user && user.authenticate(params[:user][:password])
+      render json: { 
+        user: user,
+        token: Auth.create_token({ username: user.username, id: user.id, email: user.email }) }
     else
       render json: { errors: { message: "Unable to find a user with that email or password" } }, status: 500
     end
