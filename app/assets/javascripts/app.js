3c90c86344a98f59aa3f41fe9097015334845b0b
Luke Ghenco <lukeghenco@gmail.com>
2017-02-10 10:56:54 -0500

    create Angular /login route

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,13 +8,19 @@
       $stateProvider
         .state('home', {
           url: '/home',
-          templateUrl: 'home/home.html'
+          templateUrl: 'home/home.html',
+          controller: 'HomeController',
         })
         .state('signup', {
           url: '/signup',
           templateUrl: 'auth/signup.html',
           controller: 'AuthController'
         })
+        .state('login', {
+          url: '/login', 
+          templateUrl: 'auth/login.html',
+          controller: 'AuthController'
+        });
 
       $urlRouterProvider.otherwise('/home');
     }]);
